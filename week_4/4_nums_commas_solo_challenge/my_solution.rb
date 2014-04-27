970973dae432f80f79eafce196716a317ec205c1
zagadooga <ashford91@gmail.com>
2014-04-27 14:48:42 -0700

    Update my_solution.rb

--- week_4/4_nums_commas_solo_challenge/my_solution.rb
+++ week_4/4_nums_commas_solo_challenge/my_solution.rb
@@ -1,24 +1,26 @@
-# U2.W4: Numbers to Commas Solo Challenge
+def separate_comma(num)
+    num = num.to_s
+    #If number is fewer than 4 characters, do nothing
+    if num.length < 4
+        return num
+    else
+      #split input into array and reverse it
+        splitted = num.split("").reverse
+            list = []
+            #place each character in list, and add a comma on every third character
+            for i in 0...splitted.length
+                if (i+1) % 3 == 1 && i != 0
+                    list.push(",")
+                end
+                list.push(splitted[i])
+            end
+            #join array back into string, return that string
+        list = list.reverse.join("")
+    end
+    return list
+end
 
-# Complete each step below according to the challenge directions and 
-# include it in this file. Also make sure everything that isn't code
-# is commented in the file.  
 
-
-# 1. Pseudocode
-
-# What is the input?
-# What is the output? (i.e. What should the code return?)
-# What are the steps needed to solve the problem?
-
-
-# 2. Initial Solution
-
-
-
-
-# 3. Refactored Solution
-
-
-
-# 4. Reflection 
+#This challenge was fairly challenging for me, especially because I was using a language that I am still learning.
+#However, after a while I was able to get the hang of the syntax and put out a product that both worked but
+#was also as simple as possible. I didn't want any extra features in this code, just the essentials.
