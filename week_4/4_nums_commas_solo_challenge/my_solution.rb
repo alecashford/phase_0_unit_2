f2e5598bdd9d8a151e83127dae8fc61998899dc3
zagadooga <ashford91@gmail.com>
2014-04-27 15:42:13 -0700

    Update my_solution.rb

--- week_4/4_nums_commas_solo_challenge/my_solution.rb
+++ week_4/4_nums_commas_solo_challenge/my_solution.rb
@@ -1,26 +1,30 @@
 def separate_comma(num)
     num = num.to_s
-    #If number is fewer than 4 characters, do nothing
     if num.length < 4
         return num
     else
-      #split input into array and reverse it
         splitted = num.split("").reverse
             list = []
-            #place each character in list, and add a comma on every third character
             for i in 0...splitted.length
                 if (i+1) % 3 == 1 && i != 0
                     list.push(",")
                 end
                 list.push(splitted[i])
             end
-            #join array back into string, return that string
         list = list.reverse.join("")
     end
     return list
 end
 
 
-#This challenge was fairly challenging for me, especially because I was using a language that I am still learning.
-#However, after a while I was able to get the hang of the syntax and put out a product that both worked but
-#was also as simple as possible. I didn't want any extra features in this code, just the essentials.
+#This challenge took me a fair ammount of time, until I hit upon a development strategy
+#that seemed to work really well for me. Often I start with a good idea of how I'm
+#going to figure out the problem, and as I put it into code, it becomes apparent
+#that what I thought would be a brilliant plan is acutally not going to work.
+#By that point, it's often difficult to salvage the problem since I'm working with
+#so much legacy code, and I just have to start from new. This can be lessened by
+#starting with pseudocode, but I've found it more effective to write very small
+#chunks of code, run it, check if it works and is doing what I want it to, and then
+#writing the next part. Rinse, repeat. The process is relatively time consuming,
+#but is, on the whole, much more efficient and effective than any other strategy
+#I've come into contact with at this point.
