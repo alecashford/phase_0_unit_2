c447165e119fbcfb690574536ba52f54ed87e723
zagadooga <ashford91@gmail.com>
2014-04-27 14:40:30 -0700

    Update my_solution.rb

--- week_4/1_mathy_ruby_intro/harder_calculate_median/my_solution.rb
+++ week_4/1_mathy_ruby_intro/harder_calculate_median/my_solution.rb
@@ -1,25 +1,14 @@
-# U2.W4: SOLO CHALLENGE Calculate the Median!
-
-# Complete each step below according to the challenge directions and 
-# include it in this file. Also make sure everything that isn't code
-# is commented in the file.  
-
-# I worked on this challenge [by myself, with: ].
-
-# 1. Pseudocode
-
-# What is the input?
-# What is the output? (i.e. What should the code return?)
-# What are the steps needed to solve the problem?
+def median(arr)
+    arr = arr.sort
+    if arr.length % 2 != 0
+        return arr[arr.length / 2.0]
+    else
+        return (arr[((arr.length / 2) - 1)] + arr[(arr.length / 2)]) / 2.0
+    end     
+end
 
 
-# 2. Initial Solution
-
-
-
-
-# 3. Refactored Solution
-
-
-
-# 4. Reflection 
+#I found this challenge fairly easy, as I have done similar stuff in other languages and recently in javascript,
+#so the fundamentals of the challenge were the same, the only differences were working with floating points and
+#not integers. I didn't really learn much on this challenge, but it helps just to work like this in ruby, as I
+#am still getting used to the syntax.
