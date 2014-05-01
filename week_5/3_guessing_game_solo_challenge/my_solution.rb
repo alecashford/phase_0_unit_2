233df66eca27a8e9a48d3aadcdb545543aa84f26
zagadooga <ashford91@gmail.com>
2014-04-30 20:45:24 -0700

    Update my_solution.rb

--- week_5/3_guessing_game_solo_challenge/my_solution.rb
+++ week_5/3_guessing_game_solo_challenge/my_solution.rb
@@ -1,40 +1,47 @@
 # U2.W5: Build a simple guessing game SOLO CHALLENGE
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself.
 
 # 2. Pseudocode
 
-# Input:
-# Output:
-# Steps:
+#define class variable answer
+#define method guess which takes in @answer and outputs low, correct, or equal
+#using if, elsif, and else.
+#define method solved? that returns true if input == correct, and false otherwise
 
 
 # 3. Initial Solution
 
 class GuessingGame
   def initialize(answer)
-    # Your initialization code goes here
+    @answer = rand(answer) + 1
   end
-  
-  # Make sure you define the other required methods, too
+  def guess(guess)
+    @guess = guess
+    if @guess < @answer
+        return :low
+    elsif @guess > @answer
+        return :high
+    else
+        return :correct
+    end
+  end
+  def solved?
+      if @guess == @answer
+          return true
+      else
+          return false
+      end
+  end
 end
 
-
-
-
-# 4. Refactored Solution
-
-
-
-
-
-
-# 1. DRIVER TESTS GO BELOW THIS LINE
-
-
-
-
-
-
 # 5. Reflection 
+
+#I'm actually quite confused by this one. My code seems to work, and does everything
+#the criteria on the github asks it to do, yet it does not seem to want to pass the
+#driver tests. I keep getting the same 3 error messages (including the enigmatic
+#returns :correct when the guess is correct). I have been racking my brains over
+#how this is even posssible. Nonetheless, I'm pretty proud of this work, it works
+#like I expected it to work and I learned more about the :key feature of ruby,
+#which is definitely something I will use more in the future.
