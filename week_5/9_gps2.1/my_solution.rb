09bcda1cd7602432f3ae0383abcf2747598c34f2
zagadooga <ashford91@gmail.com>
2014-05-04 16:38:11 -0700

    Update my_solution.rb

--- week_5/9_gps2.1/my_solution.rb
+++ week_5/9_gps2.1/my_solution.rb
@@ -1,22 +1,52 @@
-# U2.W5: Bakery Challenge GPS
+# U2.W5: The Bakery Challenge (GPS 2.1)
 
-# I worked on this challenge with: 
+# Your Names
+# 1) Antonio Perez
+# 2) Alec Ashford
 
+ # This is the file you should end up editing. 
+ 
+# defines method that takes 2 variables to calculate the number of stuff they need to bake
+def bakery_num(num_of_people, fav_food)
+  servings = {"pie" => 8, "cake" => 6, "cookie" => 1} 
+  pie_qty = 0
+  cake_qty = 0
+  cookie_qty = 0
+  if servings[fav_food] == nil
+    raise ArgumentError.new("We don't have that")
+  else
+  fav_food_qty = servings.values_at(fav_food)[0]
+  if num_of_people % fav_food_qty == 0
+    num_of_food = num_of_people / fav_food_qty
+    return "You need to make #{num_of_food} #{fav_food}(s)."
+  else
+      
+    # OUR SOLUTION  
+    if fav_food == "cake"
+        cake_qty = num_of_people / servings["cake"]
+        pie_qty = 0
+        cookie_qty = num_of_people % servings["cake"]
+    elsif fav_food == "pie"
+        pie_qty = num_of_people / servings["pie"]        
+        if num_of_people % servings["pie"] >= servings["cake"]
+            cake = 1
+            cookie_qty = num_of_people % servings["pie"] - servings["cake"]
+    else
+        cookie_qty = num_of_people % servings["pie"]
+    end
+    
+    else
+        cookie_qty = num_of_people
+    end
+    
+        #END OF SOLUTION
+    return "You need to make #{pie_qty} pie(s), #{cake_qty} cake(s), and #{cookie_qty} cookie(s)."
+    end
+  end
+end
+ 
 
-
-
-
-
-# Our Refactored Solution
-
-
-
-
-
-
-
-
-
+#-----------------------------------------------------------------------------------------------------
 #DRIVER CODE-- DO NOT MODIFY ANYTHING BELOW THIS LINE (except in the section at the bottom)
 # These are the tests to ensure it's working. 
 # These should all print true if the method is working properly.
@@ -28,12 +58,5 @@ p bakery_num(130, "pie") == "You need to make 16 pie(s), 0 cake(s), and 2 cookie
 # p bakery_num(3, "apples") # this will raise an ArgumentError
 
 # You SHOULD change this driver code. Why? Because it doesn't make sense.
-p bakery_num(41, "cake") == "You need to make 5 pie(s), 0 cake(s), and 1 cookie(s)." # WHAAAAAT? I thought I said I wanted cake!
-
-
-
-
-#  Reflection 
-
-
+p bakery_num(41, "cake") == "You need to make 0 pie(s), 6 cake(s), and 5 cookie(s)."
 
